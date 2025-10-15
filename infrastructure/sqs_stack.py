4808396cde6ac2c6d1735fb2565e24c7771a3244
harshitsinghai <harshitsinghai77@gmail.com>
2025-10-15 10:37:33 +0530

    Add dlq to the sqs and limit retry

--- infrastructure/sqs_stack.py
+++ infrastructure/sqs_stack.py
@@ -9,13 +9,25 @@ from constructs import Construct
 class NemoAISqsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        
+        dlq = sqs.Queue(
+            self, "NemoAIDlq",
+            queue_name="nemo-ai-tasks-dlq.fifo",
+            fifo=True,
+            content_based_deduplication=True,
+            retention_period=Duration.days(7)  # Optional: how long to keep failed messages
+        )
 
         queue = sqs.Queue(
             self, "NemoAIQueue",
             queue_name="nemo-ai-tasks.fifo",
             fifo=True,
             content_based_deduplication=True,
-            visibility_timeout=Duration.seconds(900)
+            visibility_timeout=Duration.seconds(1200),
+            dead_letter_queue=sqs.DeadLetterQueue(
+                max_receive_count=1,
+                queue=dlq
+            )
         )
 
         CfnOutput(self, "NemoAIQueueArn", value=queue.queue_arn, export_name="NemoAIQueueArn")
